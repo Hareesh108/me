0c8dcf8c3bb73f021787af26c5f0e0a6394ea2cf
hareesh <hareeshbhittam@gmail.com>
2025-06-05 21:31:15 +0530

    Removed width

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import ContactPage from "@/components/contact";
 import Footer from "@/components/footer";
-import { FullScreen } from "@/components/full-screen";
 import LandingPage from "@/components/landing-page";
 import ProjectPage from "@/components/project";
 import WorkPage from "@/components/work";
@@ -8,9 +7,7 @@ import WorkPage from "@/components/work";
 export default function Home() {
   return (
     <>
-      <FullScreen className="w-full xl:w-[85%] mx-auto">
-        <LandingPage />
-      </FullScreen>
+      <LandingPage />
 
       <ProjectPage />
 
